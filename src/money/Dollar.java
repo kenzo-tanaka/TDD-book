053e2b47cf435963e333ac71ea69abdef887cbaf
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-27 21:50:26 +0900

    最初のテストケースを作成

---
+++ src/money/Dollar.java
@@ -0,0 +1,8 @@
+package money;
+
+public class Dollar {
+
+	public Dollar(int i) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+}
---
+++ src/money/MoneyTest.java
@@ -0,0 +1,17 @@
+package money;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+	@Test
+	public void testMulti() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+	
+
+}
