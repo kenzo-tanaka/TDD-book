b9bec49edc2651066191b45b608db434c2dfd8b9
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-28 10:58:40 +0900

    refactor: timesメソッド共通化とサブクラス削除の第一歩
    timesメソッドがDollar, Francで重複しているのでこれを共通化したいと
    考えている。またDollar, Francはそもそも大した仕事をしていないので
    削除したいとも考えている。
    
    なので、Dollar削除の第一歩としてMoneyにファクトリメソッドを定義

--- src/money/Money.java
+++ src/money/Money.java
@@ -8,4 +8,8 @@ public class Money {
 		return amount == money.amount
 				&& getClass().equals(money.getClass());
 	}
+	
+	static Dollar dollar(int amount) {
+		return new Dollar(amount);
+	}
 }
--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -8,7 +8,7 @@ class MoneyTest {
 
 	@Test
 	public void testMulti() {
-		Dollar five = new Dollar(5);
+		Dollar five = Money.dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
