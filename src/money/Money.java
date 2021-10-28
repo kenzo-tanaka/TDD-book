52f10a2105a429c438fe757147ad91ff19a34d04
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-28 15:47:02 +0900

    green: Expressionインターフェースを実装したSumクラスを実装

--- src/money/Money.java
+++ src/money/Money.java
@@ -9,7 +9,7 @@ class Money implements Expression {
 	}
 	
 	Expression plus(Money addend) {
-		return new Money(amount + addend.amount, currency);
+		return new Sum(this, addend);
 	}
 	
 	Money(int amount, String currency) {
