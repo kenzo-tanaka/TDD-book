f065139ba867c2268b5e09bfe830b315bb00f48f
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-28 11:26:12 +0900

    refactor: currencyメソッドを抽象に引き上げ

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,14 +1,9 @@
 package money;
 
 public class Dollar extends Money {
-	private String currency;
 	public Dollar(int amount) {
 		this.amount = amount;
 		this.currency = "USD";
-	}
-	
-	String currency() {
-		return currency;
 	}
 	
 	Money times(int multiplier) {
