680ba0b945941d440a889b838151971210f2cd70
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-28 11:21:30 +0900

    green: currencyメソッドを生やしてテストをパス

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,8 +1,15 @@
 package money;
 
 public class Franc extends Money {
+	private String currency;
+	
 	public Franc(int amount) {
 		this.amount = amount;
+		this.currency = "CHF";
+	}
+	
+	String currency() {
+		return currency;
 	}
 	
 	Money times(int multiplier) {
