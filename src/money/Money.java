7e3df73cd2232dc008bfc8cba49be76fef2b6f6d
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-28 11:02:20 +0900

    refactor: Moneyクラスをabstractクラスに変更し、timesメソッドを定義

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,7 +1,8 @@
 package money;
 
-public class Money {
+abstract class Money {
 	protected int amount;
+	abstract Money times(int multiplier);
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
