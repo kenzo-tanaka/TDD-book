9c084c01e7b3c93704e29a8fdf09a9907c190f06
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-28 15:27:53 +0900

    red: 換算の概念をテストに導入
    通貨の換算の概念を入れていきたいので、それをまずはテストで表現
    
    P.83
    ExpressionはMoneyのように振る舞うが2つのMoneyの合計を表現する
    ようなオブジェクト。
    
    P.84
    Bankは変換を行う責務をもたせている。
    
    - Expressionは中核をなすオブジェクトでそれ以外のことを知らないようにしたい
    - Expressionにもそれをさせようとすると際限なく太りそうである

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -2,6 +2,8 @@ package money;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.beans.Expression;
+
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
@@ -29,7 +31,10 @@ class MoneyTest {
 	
 	@Test
 	public void testSimpleAddition() {
-		Money sum = Money.dollar(5).plus(Money.dollar(5));
-		assertEquals(Money.dollar(10), sum);
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(10), reduced);
 	}
 }
