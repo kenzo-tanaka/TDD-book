dff841e6af14ead7c29561f646fc6dcde02c4bfb
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-30 16:53:48 +0900

    refactor: testRunningメソッド->testTemplateMethod 不要になったsetUpメソッドを削除
    P.158
    あまり理解していない

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -20,16 +20,10 @@ class WasRun(TestCase):
     self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
-  def setUp(self):
-    self.test = WasRun("testMethod")
+  def testTemplateMethod(self):
+    test = WasRun("testMethod")
+    test.run()
+    assert("setUp testMethod " == test.log)
 
-  def testRunning(self):
-    self.test.run()
-    assert(self.test.wasRun)
-    
-  def testSetUp(self):
-    self.test.run()
-    assert("setUp testMethod " == self.test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
