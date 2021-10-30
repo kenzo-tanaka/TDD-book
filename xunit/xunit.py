c0e98f0ee82804f07458046448e77f39ab990d94
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-10-30 15:12:28 +0900

    refactor: テストのセットアップをチェックするためのテストを追加

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,24 +1,33 @@
 class TestCase:
-    def __init__(self, name):
-        self.name = name
-    
-    def run(self):
-      method = getattr(self, self.name)
-      method()
+  def __init__(self, name):
+    self.name = name
+
+  def setUp(self):
+    pass  
+  
+  def run(self):
+    self.setUp()
+    method = getattr(self, self.name)
+    method()
 
 class WasRun(TestCase):
-  def __init__(self, name):
-      self.wasRun = None
-      super().__init__(name)
-  
+  def setUp(self):
+    self.wasRun = None
+    self.wasSetUp = 1
+
   def testMethod(self):
-      self.wasRun = 1
+    self.wasRun = 1
 
 class TestCaseTest(TestCase):
   def testRunning(self):
     test = WasRun("testMethod")
-    assert(not test.wasRun)
     test.run()
     assert(test.wasRun)
+    
+  def testSetUp(self):
+    test = WasRun("testMethod")
+    test.run()
+    assert(test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
