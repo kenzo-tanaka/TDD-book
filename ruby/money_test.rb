f22c53b207dd4070dca9b2f4a1dcd83ec6cd7e19
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-03 18:40:48 +0900

    Francを追加
    多国籍の通貨の計算をする前にDollarと同じような振る舞いをするFranを導入したい

--- ruby/money_test.rb
+++ ruby/money_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative 'dollar'
+require_relative 'franc'
 
 class MoneyTest < Minitest::Test
   def test_multiplication
@@ -11,5 +12,11 @@ class MoneyTest < Minitest::Test
   def test_equality
     assert_equal(true, Dollar.new(5) == (Dollar.new(5)))
     assert_equal(false, Dollar.new(5) == (Dollar.new(6)))
+  end
+
+  def test_franc_multiplication
+    five = Franc.new(5)
+    assert_equal Franc.new(10), five.times(2)
+    assert_equal Franc.new(15), five.times(3)
   end
 end
