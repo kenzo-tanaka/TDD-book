b75daaf1d079959faa59b66717a7e44d10a58f28
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-03 19:05:29 +0900

    ✅ 第8章 DONE

--- ruby/money.rb
+++ ruby/money.rb
@@ -8,4 +8,8 @@ class Money
   def self.dollar(amount)
     Dollar.new(amount)
   end
+
+  def self.franc(amount)
+    Franc.new(amount)
+  end
 end
--- ruby/money_test.rb
+++ ruby/money_test.rb
@@ -13,14 +13,14 @@ class MoneyTest < Minitest::Test
   def test_equality
     assert_equal(true, Money.dollar(5) == (Money.dollar(5)))
     assert_equal(false, Money.dollar(5) == (Money.dollar(6)))
-    assert_equal(false, Franc.new(5) == (Franc.new(6)))
-    assert_equal(false, Franc.new(5) == (Franc.new(6)))
-    assert_equal(false, Franc.new(5) == (Money.dollar(5)))
+    assert_equal(false, Money.franc(5) == (Money.franc(6)))
+    assert_equal(false, Money.franc(5) == (Money.franc(6)))
+    assert_equal(false, Money.franc(5) == (Money.dollar(5)))
   end
 
   def test_franc_multiplication
-    five = Franc.new(5)
-    assert_equal Franc.new(10), five.times(2)
-    assert_equal Franc.new(15), five.times(3)
+    five = Money.franc(5)
+    assert_equal Money.franc(10), five.times(2)
+    assert_equal Money.franc(15), five.times(3)
   end
 end
