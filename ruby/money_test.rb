45f041e14710548d67fdba555eda17f8b9d060fc
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-03 18:30:36 +0900

    テストメソッドの命名が適切ではないので修正

--- ruby/money_test.rb
+++ ruby/money_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative 'dollar'
 
 class MoneyTest < Minitest::Test
-  def test_sample
+  def test_multiplication
     five = Dollar.new(5)
     product = five.times 2
     assert_equal 10, product.amount
