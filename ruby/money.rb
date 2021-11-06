ffaef707615166a33a692c4f2760678d8e9dab97
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-06 11:51:55 +0900

    シンプルな加算をするための実装を追加
    ここでは実装が明白なので一気に実装した。

--- ruby/money.rb
+++ ruby/money.rb
@@ -14,6 +14,10 @@ class Money
     Money.new(amount * multiplier, currency)
   end
 
+  def plus(addend)
+    Money.new(amount + addend.amount, currency)
+  end
+
   def self.dollar(amount)
     Money.new(amount, 'USD')
   end
