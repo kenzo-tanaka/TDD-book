eecfdfa4d7fe9e0d89238ebddf239aa6bf4c9485
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-06 11:44:48 +0900

    DollarとFrancの実装を削除
    すべてMoneyに実装を引き上げたのでサブクラスの実装は不要になった

--- ruby/money_test.rb
+++ ruby/money_test.rb
@@ -1,7 +1,5 @@
 require 'minitest/autorun'
 require_relative 'money'
-require_relative 'dollar'
-require_relative 'franc'
 
 class MoneyTest < Minitest::Test
   def test_multiplication
@@ -13,23 +11,11 @@ class MoneyTest < Minitest::Test
   def test_equality
     assert_equal(true, Money.dollar(5) == (Money.dollar(5)))
     assert_equal(false, Money.dollar(5) == (Money.dollar(6)))
-    assert_equal(false, Money.franc(5) == (Money.franc(6)))
-    assert_equal(false, Money.franc(5) == (Money.franc(6)))
     assert_equal(false, Money.franc(5) == (Money.dollar(5)))
-  end
-
-  def test_franc_multiplication
-    five = Money.franc(5)
-    assert_equal Money.franc(10), five.times(2)
-    assert_equal Money.franc(15), five.times(3)
   end
 
   def test_currency
     assert_equal('USD', Money.dollar(1).currency)
     assert_equal('CHF', Money.franc(1).currency)
-  end
-
-  def test_different_class_equality
-    assert_equal(true, Money.new(10, 'CHF') == Franc.new(10, 'CHF'))
   end
 end
